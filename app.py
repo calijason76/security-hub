13da8e0d3a670e2bbb9b4a1c242f33bb3c68cc40
Jason Brant <54822381+calijason76@users.noreply.github.com>
2019-10-02 22:19:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ mydata = go.Pie(
     values=myvalues,
     labels=mylabels,
     marker={'colors': [color1, color2, color3, color4, color5, color6, color7, color8, color9, color10, color11, color12, color13, color14],
-            'line': {'color': 'white', 'width': 5}}
+            'line': {'color': 'white', 'width': 3}}
 )
 mylayout = go.Layout(
     title = mytitle
